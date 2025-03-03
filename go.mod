3f9b0f6f9112e48023964209f2df82b582f0e252
nebula <nebula@nebula-lighthouse.attlocal.net>
2025-03-02 21:31:41 -0800

    remove etcd

--- go.mod
+++ go.mod
@@ -5,17 +5,40 @@ go 1.22
 toolchain go1.24.0
 
 require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/bytedance/sonic v1.11.6 // indirect
+	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/coreos/etcd v3.3.27+incompatible // indirect
 	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
 	github.com/coreos/pkg v0.0.0-20240122114842-bbd7aa9bf6fb // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.25.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
+	golang.org/x/arch v0.8.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
 )
 
 require (
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gin-gonic/gin v1.10.0
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
@@ -31,6 +54,6 @@ require (
 	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55 // indirect
 	google.golang.org/grpc v1.26.0 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,235 @@
 package main
 
 import (
-	"github.com/humbertovnavarro/orion/pkg/lighthouse"
-	"github.com/sirupsen/logrus"
+	"encoding/json"
+	"net"
+
+	"github.com/boltdb/bolt"
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
 )
 
+const CONFIG_PATH = "/etc/nebula"
+
 func main() {
-	err := lighthouse.SeedCertificates()
+	dbPath := CONFIG_PATH + "/nebula.db"
+	db, err := bolt.Open(dbPath, 0600, nil)
 	if err != nil {
-		logrus.Error(err)
-		logrus.Error("If this is the first time you started orion - you need to generate a CA cert")
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.POST("/lighthouses", func(c *gin.Context) {
+		privateAddress := c.Query("privateAddress")
+		privateNetAddress := net.ParseIP(privateAddress)
+		if privateNetAddress == nil {
+			c.JSON(400, gin.H{"error": "address is invalid"})
+			return
+		}
+		publicAddress := c.Query("publicAddress")
+		publicNetAddress := net.ParseIP(publicAddress)
+		if publicNetAddress == nil {
+			c.JSON(400, gin.H{"error": "address is invalid"})
+			return
+		}
+
+		privateAddress = privateNetAddress.String()
+		publicAddress = publicNetAddress.String()
+
+		err = db.Update(func(tx *bolt.Tx) error {
+			b, err := tx.CreateBucketIfNotExists([]byte("lighthouses"))
+			if err != nil {
+				return err
+			}
+			err = b.Put([]byte(privateAddress), []byte(publicAddress))
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+
+		if err != nil {
+			c.JSON(500, gin.H{"error": "internal error"})
+			log.Error(err)
+			return
+		}
+
+		c.JSON(200, gin.H{"publicAddress": publicAddress, "privateAddress": privateAddress})
+	})
+
+	type LighthouseMapping struct {
+		PublicAddress  string `json:"publicAddress"`
+		PrivateAddress string `json:"privateAddress"`
 	}
 
+	r.GET("/lighthouses", func(c *gin.Context) {
+		var lighthouses []LighthouseMapping
+		err := db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("lighthouses"))
+			if b == nil {
+				return nil
+			}
+			err = b.ForEach(func(k, v []byte) error {
+				lighthouses = append(lighthouses, LighthouseMapping{PublicAddress: string(v), PrivateAddress: string(k)})
+				return nil
+			})
+			return err
+		})
+		if err != nil {
+			c.JSON(500, gin.H{"error": "internal error"})
+			log.Error(err)
+			return
+		}
+		c.JSON(200, gin.H{"lighthouses": lighthouses})
+	})
+
+	r.DELETE("/lighthouses", func(c *gin.Context) {
+		privateAddress := c.Query("privateAddress")
+		privateNetAddress := net.ParseIP(privateAddress)
+		if privateNetAddress == nil {
+			c.JSON(400, gin.H{"error": "address is invalid"})
+			return
+		}
+
+		err = db.Update(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("lighthouses"))
+			if b == nil {
+				return nil
+			}
+			err = b.Delete([]byte(privateNetAddress.String()))
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+
+		if err != nil {
+			c.JSON(500, gin.H{"error": "internal error"})
+			log.Error(err)
+			return
+		}
+
+		c.JSON(200, gin.H{"privateAddress": privateAddress})
+	})
+
+	type FirewallRule struct {
+		Port   string   `json:"port"`
+		Proto  string   `json:"proto"`
+		Groups []string `json:"groups"`
+	}
+
+	r.GET("/lighthouses/firewall", func(c *gin.Context) {
+		privateAddress := c.Query("privateAddress")
+		privateNetAddress := net.ParseIP(privateAddress)
+		if privateNetAddress == nil {
+			c.JSON(400, gin.H{"error": "address is invalid"})
+			return
+		}
+
+		privateAddress = privateNetAddress.String()
+
+		firewallTable := map[string][]FirewallRule{}
+
+		err := db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("lighthouses"))
+			if b == nil {
+				return nil
+			}
+			firewallTableJson := b.Get([]byte(privateAddress + "_firewall"))
+			if firewallTableJson != nil {
+				err = json.Unmarshal(firewallTableJson, &firewallTable)
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		})
+
+		if err != nil {
+			c.JSON(500, gin.H{"error": "internal error"})
+			log.Error(err)
+			return
+		}
+
+		c.JSON(200, firewallTable)
+	})
+
+	r.POST("/lighthouses/firewall", func(c *gin.Context) {
+		privateAddress := c.Query("privateAddress")
+		privateNetAddress := net.ParseIP(privateAddress)
+
+		port := c.Query("port")
+		proto := c.Query("proto")
+		groups := c.QueryArray("groups")
+
+		if port == "" || proto == "" || len(groups) == 0 || privateNetAddress == nil {
+			c.JSON(400, gin.H{"error": "invalid parameters"})
+			return
+		}
+
+		if privateNetAddress == nil {
+			c.JSON(400, gin.H{"error": "address is invalid"})
+			return
+		}
+
+		privateAddress = privateNetAddress.String()
+
+		firewallRule := FirewallRule{Port: port, Proto: proto, Groups: groups}
+
+		if err != nil {
+			c.JSON(500, gin.H{"error": "internal error"})
+			log.Error(err)
+			return
+		}
+
+		firewallTable := map[string][]FirewallRule{}
+
+		err = db.Update(func(tx *bolt.Tx) error {
+			b, err := tx.CreateBucketIfNotExists([]byte("lighthouses"))
+			if err != nil {
+				return err
+			}
+			firewallTableJson := b.Get([]byte(privateAddress))
+			if firewallTableJson != nil {
+				err = json.Unmarshal(firewallTableJson, &firewallTable)
+				if err != nil {
+					return err
+				}
+				firewallTable[port] = append(firewallTable[port], firewallRule)
+				if err != nil {
+					return err
+				}
+			} else {
+				firewallTable := map[string][]FirewallRule{port: {firewallRule}}
+				firewallTableJson, err = json.Marshal(firewallTable)
+				if err != nil {
+					return err
+				}
+			}
+			err = b.Put([]byte(privateAddress+"_firewall"), firewallTableJson)
+			c.JSON(200, firewallTable)
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+
+		if err != nil {
+			c.JSON(500, gin.H{"error": "internal error"})
+			log.Error(err)
+			return
+		}
+
+		c.JSON(200, gin.H{"rule": firewallTable})
+	})
+
+	r.Run(":8080")
 }
